1827dbed76ce34c807e6edf2244ffb608a24f169
paulm <paulm@ed00cf0f-cdb0-1a42-b61b-b5e9e26c2d77>
2011-05-16 18:10:02 +0000

    Adding missing file
    git-svn-id: svn://gary/repos-devtest/org.surveyresults@1929 ed00cf0f-cdb0-1a42-b61b-b5e9e26c2d77

---
+++ grails-app/services/org/surveyresults/ReviewService.groovy
@@ -0,0 +1,25 @@
+package org.surveyresults
+
+
+class ReviewService {
+
+    static transactional = false
+
+    def reviewsLeftToComplete(def teamMember) {
+		def done = reviewsCompleted(teamMember)
+		def all = reviewsForOtherTeamMembers(teamMember)
+		all.findAll { r->done.find {d-> d.id==r.id } == null}
+    }
+	
+	def reviewsCompleted(def teamMember){
+		def reviewsCompleted = Review.withCriteria(){
+			evaluations{
+				eq("responder.id",teamMember.id)
+			}
+		}
+	}
+	
+	def reviewsForOtherTeamMembers(def teamMember){
+		Review.findAllByRevieweeNotEqual(teamMember)
+	}
+}

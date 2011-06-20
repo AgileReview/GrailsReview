dd86560601f2651e4caf875128ba01d01dd27b28
Paul McCallick <paul@mccallick.com>
2011-06-20 13:55:34 -0700

    scrubbed out real emails

--- grails-app/conf/BootStrap.groovy
+++ grails-app/conf/BootStrap.groovy
@@ -8,16 +8,8 @@ class BootStrap {
 			new Role(name:'Manager').save(failOnError:true)
 		}
 		if(!TeamMember.count()){
-			new TeamMember(name:'Patrick Escarcega',role:Role.findByName('Dev'),email:'pescarcega@paraport.com',password:'patrick').save(failOnError:true)
-			new TeamMember(name:'Dave Perry',role:Role.findByName('Dev'),email:'dperry@paraport.com',password:'dave').save(failOnError:true)
-			//new TeamMember(name:'Josh Rolstad',role:Role.findByName('Dev'),email:'jrolstad@paraport.com',password:'josh').save(failOnError:true)
-			//new TeamMember(name:'Sumair Pervaiz',role:Role.findByName('Dev'),email:'spervaiz@paraport.com',password:'sumair').save(failOnError:true)
-			//new TeamMember(name:'Mary Panza',role:Role.findByName('QA'),email:'mpanza@paraport.com',password:'mary').save(failOnError:true)
-			//new TeamMember(name:'Michelle Wu',role:Role.findByName('QA'),email:'mwu@paraport.com',password:'michelle').save(failOnError:true)
-			//new TeamMember(name:'Nathan Flint',role:Role.findByName('QA'),email:'nflint@paraport.com',password:'nathan').save(failOnError:true)
-			//new TeamMember(name:'Laurie Soetanto',role:Role.findByName('QA'),email:'lsoetanto@paraport.com',password:'laurie').save(failOnError:true)
-			//new TeamMember(name:'Eric Peterson',role:Role.findByName('QA'),email:'epeterson@paraport.com',password:'eric').save(failOnError:true)
-			//new TeamMember(name:'Paul McCallick',role:Role.findByName('Manager'),email:'pmccallick@paraport.com',password:'paul').save(failOnError:true)
+			new TeamMember(name:'Patrick',role:Role.findByName('Dev'),email:'patrick@p.com',password:'patrick').save(failOnError:true)
+			new TeamMember(name:'Dave',role:Role.findByName('Dev'),email:'dave@d.com',password:'dave').save(failOnError:true)
 		}
 		if(!Question.count()){
 			new Question(text:'Works well with a team').save(failOnError:true)
